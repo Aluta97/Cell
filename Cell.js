5ecbf84006466fdd4aa2bb6900648e159ca853ce
Lusindiso <lusindiso@projectcodex.co>
2016-04-07 12:26:23 +0200

    second commit

--- Cell.js
+++ Cell.js
@@ -18,31 +18,50 @@ person4 =  "Gus sent: Around 150MB. I have the receipts if you need more precise
 var person1 =  person1.replace(" sent", " ")
                       .replace("It was", " ")
                       .split(".")
-    console.log(person1);
+    // console.log(person1);
 
 var person2 = person2.replace(" sent", " ")
                       .replace("Damn,", " ")
                       .replace("it was like", " ")
                       .split(".");
-    console.log(person2)
+    // console.log(person2)
 
 var person3 = person3 .replace(" sent", " ")
                       .replace("I think", " ")
                       .split(".");
-      console.log(person3)
+      // console.log(person3)
 
 
 var person4 = person4.replace(" sent", " ")
                       .replace("Around", " ")
                       .split(".");
-    console.log(person4)
 
-for (var i = 0; i < person1.length; i++){
+    // console.log(person4)
+
+var people = [person1, person2, person3, person4];
+          // console.log(people);
+
+
+var x = [];
+
+for (var i = 0; i < people.length; i++){
 //printing out only the indexes
-        console.log(person1[0])
-        console.log(person2[0])
-        console.log(person3[0])
-        console.log(person4[0]);
 
-
+  // var walter = person1[0]
+  // var jesse = person2[0]
+  // var saul = person3[0]
+  // var gus = person4[0]
 }
+//Printing index 0 of every Array
+
+// console.log(person1[0]);
+// console.log(person2[0]);
+// console.log(person3[0]);
+// console.log(person4[0]);
+
+//pushing every index into an array
+x.push(person1[0],
+       person2[0],
+       person3[0],
+       person4[0]);
+ console.log(x);
